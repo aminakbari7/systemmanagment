b02e9067f5baa681dc7e57ca849db2640059d14e
Amin <aminakbari733@gmail.com>
2024-12-02 20:07:04 +0330

    Update Program.cs

--- systemmanagment/Program.cs
+++ systemmanagment/Program.cs
@@ -17,6 +17,7 @@ namespace systemmanagment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Main());
+            
         }
     }
 }
