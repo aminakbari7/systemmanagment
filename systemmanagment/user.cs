eea816f07981dcb0b95445b8573ab5cbe76d98a3
Amin <aminakbari733@gmail.com>
2024-12-09 12:16:15 +0330

    Create user.cs

---
+++ systemmanagment/user.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemmanagment
+{
+    internal class user
+    {
+        public int user_Id { get; set; }
+        public int user_roleid { get; set; }
+        public string user_username { get; set; }
+        public string user_name { get; set; }
+        public string user_family { get; set; }
+        public string user_password { get; set; }
+        public string user_lastlogin { get; set; }
+
+    }
+}
