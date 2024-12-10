d17fb36fe5960a8fd0a9d2a80ff56fdc002d2fb3
Amin <aminakbari733@gmail.com>
2024-12-10 09:06:05 +0330

    Create ClassUser.cs

---
+++ systemmanagment/ClassUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemmanagment
+{
+    internal class ClassUser
+    {
+        public int Id { get; set; }
+        public int Role_Id { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public string Name { get; set; }
+
+        public string Family { get; set; }
+        public string LastLogin{ get; set; }
+        
+    }
+}
